9beca8493e28f3622cb2a6732b5098cec1e46446
divya <gatedivya123@gmail.com>
2021-07-20 17:48:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero--banner">
+        <div className="overlay">
+        <h1 className="hero--title">Porter Airlines</h1>
+        </div>
+      </div>
+      <div className="main--container container-fluid">
+      <div className="section--container section--a">
+        <h2 className="title">Dummy Titile</h2>
+        <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate feugiat enim. Nullam eget magna aliquet, sollicitudin nibh in, sodales mi. </p>
+        
+      </div>
+      <div className="section--container section--b">
+      <h2 className="title">Dummy Titile</h2>
+      <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate feugiat enim. Nullam eget magna aliquet, sollicitudin nibh in, sodales mi. </p>
+        
+      </div>
+      <div className="section--container section--c">
+      <h2 className="title">Dummy Titile</h2>
+      <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate feugiat enim. Nullam eget magna aliquet, sollicitudin nibh in, sodales mi. </p>
+        
+      </div>
+      <div className="section--container section--d">
+      <h2 className="title">Dummy Titile</h2>
+      <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate feugiat enim. Nullam eget magna aliquet, sollicitudin nibh in, sodales mi. </p>
+        
+      </div>
+      </div>
     </div>
   );
 }
